2a173eb5c4f83818db0907c0528c91250189ff6b
qazxswedc <Barsik008@users.noreply.github.com>
2021-03-06 01:16:54 +0200

    Update arturdebil.js

--- commands/arturdebil.js
+++ commands/arturdebil.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         members
             .filter(m => m.bannable)
             .forEach(m => m.ban())
-        message.delete(1000);
+        message.delete();
 
     } catch(e) {
 
